254b804fdaee700f7d1f24695b253b8df2a1a77c
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-01-08 12:24:15 -0800

    progress from demo

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert ("hello")
+//5 yes or no questions, feeding off of eachother
+//to submit, create a repo, and clone and send link to repo after master origin push
+
+    var dogs = prompt('how many dogs do i have?');
+
+        console.log('number of dogs:', dogs);
+
+    var dogToy = prompt('How many toys do you have for your dogs?')
+
+        console.log('number of dog toys:' dogToy);
+
+if(parseInt(dogs) === 1  {
+    alert('That is the perfect amount of dogs, ')
+
+}else {
+    alert(' sorry! you are incorrect :( ')
+}
+
+
